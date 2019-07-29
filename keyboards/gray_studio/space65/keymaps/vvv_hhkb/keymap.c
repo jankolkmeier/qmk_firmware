22bbf9f4ee7a8afd83b78df0c55c827adc134d2e
Jan Kolkmeier <jankolkmeier@gmail.com>
2019-07-29 22:45:35 +0200

    macros

--- keyboards/gray_studio/space65/keymaps/vvv_hhkb/keymap.c
+++ keyboards/gray_studio/space65/keymaps/vvv_hhkb/keymap.c
@@ -28,6 +28,10 @@ enum vvv_smooth_vals {
   layer_hue
 };
 
+static bool isRecording = false;
+const uint8_t recording_hue = 248;
+const uint8_t reset_hue = 130;
+
 // Color of the slave light
 const uint8_t slave_hue = 30;
 // Color of the layers: there has to be one number for each layer!
@@ -40,20 +44,22 @@ const uint8_t layer_hues[] = {
 enum layers_idx {
     _BL, // Base Layer
     _WL, // Windows Modifiers Layer (Switched around LGUI, LALT, LCTL)
-    _FL  // Functions
+    _FL // Functions
 };
 
 enum custom_keycodes {
   VVV_DFT = SAFE_RANGE,
   VVV_RST,
-  VVV_STY
+  DYNAMIC_MACRO_RANGE
 };
+
+#include "dynamic_macro.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BL] = LAYOUT( \
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, KC_BSLS,  KC_GRV,  VVV_STY,   \
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,         KC_BSPC,  KC_PGUP,  \
-        KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,  KC_PGDN,  \
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, KC_BSLS,  KC_GRV,  KC_DEL,  \
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,         KC_BSPC,  DYN_MACRO_PLAY1,  \
+        KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,  DYN_MACRO_PLAY2,  \
         KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,         KC_RSFT,   KC_UP,  MO(_FL),  \
         MO(_FL), KC_LALT, KC_LCTL,                   KC_SPC,  KC_SPC,  KC_SPC,           KC_RALT, MO(_FL),                  KC_LEFT, KC_DOWN,  KC_RGHT   \
     ),
@@ -65,11 +71,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_LGUI, KC_LALT,                   _______, _______, _______,          _______, _______,                   _______, _______, _______   \
     ),
 [_FL] = LAYOUT( \
-        KC_POWER,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F11, KC_F12,  KC_INS,  KC_DEL,  _______,  \
-        _______, XXXXXXX, VVV_DFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SLCK, KC_BRK,  KC_UP,   XXXXXXX,          _______, KC_HOME,  \
-        _______, KC_VOLD, KC_VOLU, KC_MUTE, KC_EJCT, XXXXXXX, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT,                   KC_PENT, KC_END,   \
+    DYN_REC_STOP,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F11, KC_F12,  KC_INS,  KC_DEL,  _______,  \
+        _______, XXXXXXX, VVV_DFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SLCK, KC_BRK,  KC_UP,   XXXXXXX,          _______, DYN_REC_START1,  \
+        _______, KC_VOLD, KC_VOLU, KC_MUTE, KC_EJCT, XXXXXXX, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT,                   KC_PENT, DYN_REC_START2,   \
         _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PPLS, KC_PMNS, KC_END,  KC_PGDN, KC_DOWN,          _______, _______, VVV_RST,  \
-        VVV_RST,   _______, _______,                   _______, _______, _______,        _______, VVV_RST,                   _______, _______, _______   \
+        VVV_RST, _______, _______,                   _______, _______, _______,        _______, VVV_RST,                   _______, _______, _______   \
     ),
 };
 
@@ -84,6 +90,7 @@ void keyboard_post_init_user(void) {
 
 void matrix_init_user(void) {
   // Setup smoothing engine
+  isRecording = false;
   vvv_smooth_init(total_brightness, 0, 230, 0, 230, 0.035);
   vvv_smooth_init(type_pulse, 235, 235, 80, 255, 0.1);
   vvv_smooth_init(layer_hue, 0, layer_hues[biton32(eeconfig_read_default_layer())], 1, 255, 0.15);
@@ -103,26 +110,38 @@ void matrix_scan_user(void) {
 uint32_t layer_state_set_user(uint32_t state) {
   uint8_t layer = biton32(state);
   uint8_t default_layer = biton32(default_layer_state);
-  vvv_smooth_set_target(layer_hue, layer_hues[layer]);
-  if (layer <= default_layer) {
-    vvv_smooth_set_target(layer_hue, layer_hues[default_layer]);
+  if (isRecording) {
+    vvv_smooth_set_target(layer_hue, recording_hue);
+  } else {
+    vvv_smooth_set_target(layer_hue, layer_hues[layer]);
+    if (layer <= default_layer) {
+      vvv_smooth_set_target(layer_hue, layer_hues[default_layer]);
+    }
   }
   return state;
 }
 
 // TODO: just messing around here. Maybe visualize all modifiers?
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!process_record_dynamic_macro(keycode, record)) {
+    return false;
+  }
   switch (keycode) {
     case VVV_RST: // RESET THE KEYBOARD
       if (record->event.pressed) break;
-      rgblight_sethsv_master(248, 235, 235);
+      rgblight_sethsv_master(reset_hue, 235, 235);
       rgblight_sethsv_slave(0, 0, 0);
       reset_keyboard();
       return false;
-    case VVV_STY:
-      if (record->event.pressed) break;
-      SEND_STRING(SS_LCTRL(SS_LSFT(SS_LGUI("n"))));
-      return false;
+    case DYN_REC_START1:
+      isRecording = true;
+      break;
+    case DYN_REC_START2:
+      isRecording = true;
+      break;
+    case DYN_REC_STOP:
+      isRecording = false;
+      break;
     case VVV_DFT: // TOGGLE DEFAULT LAYER (TODO: more elegant solution here)
       if (record->event.pressed) break;
       switch (biton32(default_layer_state)) {
